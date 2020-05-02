341c3d59874cfccba8966c41dad8178e4c08fcac
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2020-05-02 03:30:04 +0000

    ddot, sdot change support

---
+++ docker_scripts/openblas_build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#cd /work/OpenBLAS; mkdir build; cd build; cmake .. ; make -j ; make install 
+apt update -y
+apt install -y libopenblas-dev
+
--- test.sh
+++ test.sh
@@ -12,11 +12,22 @@ else
 	done
 fi
 
+#DDOT
 for th in "${array[@]}"
 do
 	for (( i = $DDOT_MIN_SIZE ; i<= $DDOT_MAX_SIZE ; i=i + $DDOT_INCL_SIZE ))
 	do
 		export OMP_NUM_THREADS=$th
-		./src/blas1_stream/dot.o $i 10
+		./src/blas1_stream/dot.o $i $DDOT_TRIAL double
+	done
+done
+
+#SDOT
+for th in "${array[@]}"
+do
+	for (( i = $SDOT_MIN_SIZE ; i<= $SDOT_MAX_SIZE ; i=i + $SDOT_INCL_SIZE ))
+	do
+		export OMP_NUM_THREADS=$th
+		./src/blas1_stream/dot.o $i $SDOT_TRIAL float
 	done
 done
