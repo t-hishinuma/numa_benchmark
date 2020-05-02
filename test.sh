a1932875e688570c8c6b62cebfac3fb50b86f1f7
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2020-05-02 03:44:09 +0000

    incl size

--- test.sh
+++ test.sh
@@ -11,7 +11,7 @@ array=()
 if [ -n "$THREADS" ]; then
 	array=($THREADS)
 else
-	MAXTH=`grep cpu.cores /proc/cpuinfo | sort -u | awk '{print \$4}'` 
+	MAXTH=`grep processor /proc/cpuinfo | wc -l` 
 	for ((i=$MAXTH;i>=1;i=i/2))
 	do
 		array+=$i
@@ -22,7 +22,7 @@ fi
 if [ $DDOT_EXEC = 1 ]; then
 	for th in "${array[@]}"
 	do
-		for (( i = $DDOT_MIN_SIZE ; i<= $DDOT_MAX_SIZE ; i=i + $DDOT_INCL_SIZE ))
+		for (( i = $DDOT_MIN_SIZE ; i<= $DDOT_MAX_SIZE ; i=i$DDOT_INCL ))
 		do
 			export OMP_NUM_THREADS=$th
 			./src/blas1_stream/dot.o $i $DDOT_TRIAL double
@@ -31,10 +31,10 @@ if [ $DDOT_EXEC = 1 ]; then
 fi
 
 #SDOT
-if [ $DDOT_EXEC = 1 ]; then
+if [ $SDOT_EXEC = 1 ]; then
 	for th in "${array[@]}"
 	do
-		for (( i = $SDOT_MIN_SIZE ; i<= $SDOT_MAX_SIZE ; i=i + $SDOT_INCL_SIZE ))
+		for (( i = $SDOT_MIN_SIZE ; i<= $SDOT_MAX_SIZE ; i=i$SDOT_INCL ))
 		do
 			export OMP_NUM_THREADS=$th
 			./src/blas1_stream/dot.o $i $SDOT_TRIAL float
