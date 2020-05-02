f75d9cff8d950684c77a1c52aa2a032941a4e70a
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2020-05-02 03:34:50 +0000

    ddot, sdot change support

--- test.sh
+++ test.sh
@@ -1,4 +1,10 @@
 #!/bin/zsh
+
+if [ $# != 1 ]; then
+	echo "error \$1 is config filename"
+	exit 1
+fi
+
 . $1
 array=()
 
@@ -13,21 +19,25 @@ else
 fi
 
 #DDOT
-for th in "${array[@]}"
-do
-	for (( i = $DDOT_MIN_SIZE ; i<= $DDOT_MAX_SIZE ; i=i + $DDOT_INCL_SIZE ))
+if [ $DDOT_EXEC = 1 ]; then
+	for th in "${array[@]}"
 	do
-		export OMP_NUM_THREADS=$th
-		./src/blas1_stream/dot.o $i $DDOT_TRIAL double
+		for (( i = $DDOT_MIN_SIZE ; i<= $DDOT_MAX_SIZE ; i=i + $DDOT_INCL_SIZE ))
+		do
+			export OMP_NUM_THREADS=$th
+			./src/blas1_stream/dot.o $i $DDOT_TRIAL double
+		done
 	done
-done
+fi
 
 #SDOT
-for th in "${array[@]}"
-do
-	for (( i = $SDOT_MIN_SIZE ; i<= $SDOT_MAX_SIZE ; i=i + $SDOT_INCL_SIZE ))
+if [ $DDOT_EXEC = 1 ]; then
+	for th in "${array[@]}"
 	do
-		export OMP_NUM_THREADS=$th
-		./src/blas1_stream/dot.o $i $SDOT_TRIAL float
+		for (( i = $SDOT_MIN_SIZE ; i<= $SDOT_MAX_SIZE ; i=i + $SDOT_INCL_SIZE ))
+		do
+			export OMP_NUM_THREADS=$th
+			./src/blas1_stream/dot.o $i $SDOT_TRIAL float
+		done
 	done
-done
+fi
