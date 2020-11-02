a8ef1056437408408c123087a2aaf39946390aca
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2020-11-03 02:10:41 +0900

    get_spec script bug fix

--- src/machine_spec/get_spec.sh
+++ src/machine_spec/get_spec.sh
@@ -25,7 +25,7 @@ fi
 # GPU
 if type "nvidia-smi" > /dev/null 2>&1; then
 	GPU=`nvidia-smi -L`
-    if echo $GPU | grep "fail"; then
+    if echo $GPU | grep "fail" > /dev/null 2>&1; then
         GPU="none"
     fi
 else
